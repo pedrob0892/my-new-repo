25b36d856309b6be6bc25afb614caa16c75bdcf4
Pedro <pedrobocchini@gmail.com>
2025-04-07 14:29:31 -0300

    new commit

--- app.py
+++ app.py
@@ -3,8 +3,11 @@ import pandas as pd
 import plotly_express as px
 
 vehicles = pd.read_csv('vehicles.csv')
+st.header('Conjunto de dados de carros')
 hist_button = st.button('Criar histograma')
 if hist_button:
             st.write('Criando um histograma para o conjunto de dados de anúncios de vendas de carros')
             fig = px.histogram(vehicles, x="odometer")
             st.plotly_chart(fig, use_container_width=True)
+fig = px.scatter(vehicles, x="odometer", y="price")
+fig.show()
