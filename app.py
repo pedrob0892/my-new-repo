ee9732b96db9d641621278ab25aa92f03eafd642
Pedro <pedrobocchini@gmail.com>
2025-04-07 15:12:38 -0300

    newcommit

--- app.py
+++ app.py
@@ -7,9 +7,9 @@ st.header('Conjunto de dados de carros')
 hist_button = st.button('Clique para criar o histograma')
 if hist_button:
             st.write('Conjunto de dados de anúncios de vendas de carros')
-            fig1 = px.histogram(vehicles, x="odometer")
+            fig1 = px.histogram(vehicles, x="Distância percorrida")
             st.plotly_chart(fig1, use_container_width=True)
 dispersion_button = st.button('Clique para criar um gráfico de dispersão')
 if dispersion_button:
-            fig2 = px.scatter(vehicles, x="odometer", y="price")
+            fig2 = px.scatter(vehicles, x="Distância percorrida", y="Preço")
             st.plotly_chart(fig2, use_container_width=True)
