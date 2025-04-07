aad9da181b0cd0818e2f65a2b8f6fda1074423c5
Pedro <pedrobocchini@gmail.com>
2025-04-07 17:08:19 -0300

    newwco

--- app.py
+++ app.py
@@ -6,10 +6,11 @@ vehicles = pd.read_csv('vehicles.csv')
 st.header('Conjunto de dados de carros')
 hist_button = st.button('Clique para criar o histograma')
 if hist_button:
-            st.write('Conjunto de dados de anúncios de vendas de carros')
-            fig1 = px.histogram(vehicles, x="odometer", labels={'odometer': 'quilometragem'})
+            st.write('Número de carros por quilometragem')
+            fig1 = px.histogram(vehicles, x="odometer", labels={'odometer': 'quilometragem', 'count': 'Número de carros'})
             st.plotly_chart(fig1, use_container_width=True)
 dispersion_button = st.button('Clique para criar um gráfico de dispersão')
 if dispersion_button:
+            st.write('Preço dos carros por quilometragem')
             fig2 = px.scatter(vehicles, x="odometer", y="price", labels={'odometer': 'quilometragem', 'price': 'preço'})
             st.plotly_chart(fig2, use_container_width=True)
